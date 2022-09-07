09da5c185138e8619ffb8f6a25a094ef45c6ee0a
fernandasayuri <fernanda.ito@terceiro-sky.com.br>
2022-09-07 15:45:21 -0300

    useStade counter

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -32,9 +32,13 @@ import React, { Component, useState } from 'react';
 function App() {
   const [reverse, setReverse] = useState(false);
   const reverseClass = reverse ? 'reverse' : '';
-
- const handleClick = () => {
+  const handleClick = () => {
     setReverse(!reverse);
+  }
+  
+  const [counter, setCounter] = useState(0);
+  const handleClickCounter = () => {
+    setCounter(counter+1);
   }
 
   return (
@@ -42,6 +46,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
     
+        <h1>Contador: {counter}</h1>
+
+        <button type='button' onClick={handleClickCounter}>
+          Contador++
+        </button>
+
         <button type='button' onClick={handleClick}>
           Reverse {reverseClass}
         </button>
